78b927bbf5902e30368200efce861d83bc9be109
Bernt Andreas Eide <bernta1@msn.com>
2025-04-23 22:50:30 +0100

    Added new game listing controls

---
+++ ValveModHub.Desktop/Controls/GameControl.cs
@@ -0,0 +1,70 @@
+﻿using ValveModHub.Common.Model;
+using ValveModHub.Desktop.Services;
+
+namespace ValveModHub.Desktop.Controls;
+
+public partial class GameControl : UserControl
+{
+    private readonly Game _game;
+    private readonly Image _gameIcon;
+    private bool _hover;
+
+    public GameControl()
+    {
+        InitializeComponent();
+        DoubleBuffered = true;
+    }
+
+    public GameControl(Game game) : this()
+    {
+        _game = game;
+        _gameIcon = Image.FromFile($"{Application.StartupPath}assets/{game.Icon}.jpg");
+    }
+
+    ~GameControl()
+    {
+        _gameIcon.Dispose();
+    }
+
+    protected override void OnDoubleClick(EventArgs e)
+    {
+        base.OnDoubleClick(e);
+        SteamBrowserProtocolService.LaunchGame(_game);
+    }
+
+    protected override void OnMouseEnter(EventArgs e)
+    {
+        if (!Enabled)
+            return;
+
+        _hover = true;
+        base.OnMouseEnter(e);
+
+        Invalidate();
+    }
+
+    protected override void OnMouseLeave(EventArgs e)
+    {
+        if (!Enabled)
+            return;
+
+        _hover = false;
+        base.OnMouseLeave(e);
+
+        Invalidate();
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+
+        if (DesignMode || _game is null)
+            return;
+
+        e.Graphics.DrawImage(_gameIcon, new Rectangle(0, 0, Size.Height, Size.Height));
+        e.Graphics.DrawString(_game.Name, SystemFonts.DefaultFont, SystemBrushes.Control, new Point(Size.Height + 1, 0));
+
+        if (_hover)
+            e.Graphics.DrawRectangle(SystemPens.Control, new Rectangle(0, 0, Size.Width, Size.Height));
+    }
+}
---
+++ ValveModHub.Desktop/Controls/GameListControl.cs
@@ -0,0 +1,49 @@
+﻿using ValveModHub.Common.Model;
+
+namespace ValveModHub.Desktop.Controls;
+
+public partial class GameListControl : UserControl
+{
+    private readonly List<Game> _games;
+
+    public GameListControl()
+    {
+        InitializeComponent();
+        DoubleBuffered = true;
+    }
+
+    public GameListControl(string category, List<Game> games) : this()
+    {
+        _games = games;
+
+        var grid = new TableLayoutPanel();
+        grid.Parent = this;
+        grid.Dock = DockStyle.Fill;
+        grid.CellBorderStyle = TableLayoutPanelCellBorderStyle.None;
+
+        var row = 0;
+        var size = 20;
+
+        var labelSection = new Label();
+        labelSection.Parent = grid;
+        labelSection.Dock = DockStyle.Fill;
+        labelSection.Text = category;
+
+        grid.SetCellPosition(labelSection, new TableLayoutPanelCellPosition(0, 0));
+
+        foreach (var game in _games)
+        {
+            var gameUiControl = new GameControl(game);
+
+            gameUiControl.Parent = grid;
+            gameUiControl.Dock = DockStyle.Fill;
+
+            grid.SetCellPosition(gameUiControl, new TableLayoutPanelCellPosition(0, row + 1));
+            grid.RowStyles.Add(new RowStyle(SizeType.Absolute, size));
+
+            row++;
+        }
+
+        Height = (row * size);
+    }
+}
